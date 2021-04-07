e39ae89532426875cfe920ab6d08c0f6cebdfbdb
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2021-04-06 22:19:37 -0300

    subindo os arquivos da aula de linguagem C do dia 06//04/21

---
+++ Linguagem_C/index.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#include <locale.h>
+
+
+int main()  
+{
+	setlocale(LC_ALL,"Portuguese");
+	/*int x, y;
+
+	printf("Digite o valor de X: \n");
+	scanf("%d", &x);
+
+	printf("Digite o valor de Y: \n");
+	scanf("%d", &y);
+
+	//Estrutura do SE
+	if (x>y){
+		//instrução
+		printf("X é maior que Y!\n");	
+	}else if(x<y){
+		printf("Y é maior que X!\n");
+	}else{
+		printf("X e Y são iguais\n");
+	}*/
+
+	/*float salario, prestacao;
+	printf("Digite o valor de seu salário: \n");
+	scanf("%f", &salario);
+
+	printf("Digite o valor da prestação desejada: \n");
+	scanf("%f", &prestacao);	
+	
+	if (prestacao>=(salario*0.2)){
+		printf("Empréstimo negado.\n");
+	}else {
+		printf("Empréstimo liberado\n");
+	}*/
+
+	/*int valor, resto;
+
+	printf("Digite um valor: \n");
+	scanf("%d", &valor);
+
+	resto = valor%2;
+	
+	if (resto == 0){
+		printf("Esse número é par\n");
+	}else{
+		printf("Esse número é impar\n");
+	}*/
+
+	/*float salario, gastos;
+
+	printf("Digite o valor do seu salário: \n");
+	scanf("%f", &salario);
+
+	printf("Digite o valor dos seus gastos: \n");
+	scanf("%f", &gastos);
+
+	if(gastos <= salario) {
+		printf("Gastos dentro do orçamento.\n");
+	}else{
+		printf("Você tá gastando mais do que ganha.\n");
+	}*/
+
+	/*float nota1, nota2, media;
+
+	printf("Qual a nota1: \n");
+	scanf("%f", &nota1);
+
+	printf("Qual a nota2: \n");
+	scanf("%f", &nota2);
+
+	media = (nota1+nota2) / 2;
+
+	if (media >= 6)
+	{
+		printf("Aluno aprovado com média %f. ", media);
+	}else{
+		printf("Aluno com média %f. Irá fazer AV3.", media);
+	}*/
+
+
+	return 0;
+}

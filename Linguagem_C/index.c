cff5577c789da0472444105deaf03d6f773297cd
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2021-04-13 22:26:28 -0300

    Aula do dia 13/04/21

--- Linguagem_C/index.c
+++ Linguagem_C/index.c
@@ -186,5 +186,28 @@ int main(void)
 	Acima de 30 obeso
 	*/
 
+	float imc, altura, peso;
+
+	printf("Informe sua Altura: \n");
+	scanf("%f", &altura);
+
+	printf("Informe seu Peso: \n");
+	scanf("%f", &peso);
+
+	imc = peso / (altura*altura);
+
+	if (imc < 18.5)
+	{
+		printf("Com o IMC de %.2f você está abaixo do peso\n", imc );
+	}else if (imc >= 18.5 && imc < 25)
+	{
+		printf("Com o IMC de %.2f você está no peso ideal\n", imc );
+	}else if (imc >= 25 && imc < 30)
+	{
+		printf("Com o IMC de %.2f você está acima do peso\n", imc );
+	}else {
+		printf("Com o IMC de %.2f você está obeso\n", imc );
+	}
+
 	return 0;
 }

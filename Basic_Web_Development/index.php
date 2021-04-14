c1540f1fcc07a2d571cfc59b287be070d9e6ed38
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2021-04-13 22:29:37 -0300

    Estudando PHP

---
+++ Basic_Web_Development/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Estudando PHP</title>
+</head>
+<body>
+	<h1>Super Global Variables</h1>
+	<h2>PHP $_SERVER</h2>
+
+<?php
+
+
+?>
+</body>
+</html>

cfc7f19f74e1c1cb489807195c5c7ad910760bd8
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2021-04-24 01:31:36 -0300

    Resposta da questão 3

--- Linguagem_C/respostasTrbalho.cpp
+++ Linguagem_C/respostasTrbalho.cpp
@@ -63,6 +63,39 @@ int main(void)
 		printf("Empréstimo liberado\n");
 	}*/
 
-	
+	/*Questão 3: Uma empresa quer verificar se um empregado está qualificado para a aposentadoria ou não. Para estar em condições, um dos seguintes requisitos deve ser satisfeito: 
+	- Ter no mínimo 65 anos de idade. 
+	- Ter trabalhado no mínimo 30 anos. 
+	- Ter no mínimo 60 anos e ter trabalhado no mínimo 25 anos. 
+	Com base nas informações acima, faça um algoritmo que leia: o número do empregado (código), o ano de seu nascimento e o ano de seu ingresso na empresa. O programa deverá escrever a idade e o tempo de trabalho do empregado e a mensagem 'Requerer aposentadoria' ou 'Não requerer'.*/
+
+	int matricula, anoNascimento, anoAdmissao, idade, tempoDeServico;
+
+	printf("Informe sua matrícula: \n");
+	scanf("%d", &matricula);
+
+	printf("Informe o ano do seu nascimento: \n");
+	scanf("%d", &anoNascimento);
+
+	printf("Informe o ano da sua contratação: \n");
+	scanf("%d", &anoAdmissao);
+
+	idade = 2021 - anoNascimento;
+	tempoDeServico = 2021 - anoAdmissao;
+
+	if (idade >= 65)
+	{
+		printf("Sua idade é %da e você contribuiu %da-\n Requerer aposentadoria\n", idade, tempoDeServico);
+	}else if (tempoDeServico >= 30)
+	{
+		printf("Sua idade é %da e você contribuiu %da -\n Requerer aposentadoria\n", idade, tempoDeServico);
+	}else if (idade >= 60 && tempoDeServico >= 25)
+	{
+		printf("Sua idade é %da e você contribuiu %da -\n Requerer aposentadoria\n", idade, tempoDeServico);
+	}else {
+		printf("Sua idade é %da e você contribuiu %da -\n Não requerer aposentadoria\n", idade, tempoDeServico);
+	}
+
+
 	return 0;
 }

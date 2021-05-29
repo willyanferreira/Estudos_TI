671448ba67ddd39daabcc4f13f2cb23db81753dd
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2021-05-29 12:06:49 -0300

    Voltando aos estudos

--- Basic_Web_Development/index.php
+++ Basic_Web_Development/index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -6,8 +5,10 @@
 	<title>Estudando PHP</title>
 </head>
 <body>
+<h1>Eu estou estudando!</h1>
 <?php
-  phpinfo();
+  echo 'Editei esse arquivo em uma máquina windows usando o VS Code...';
 ?>
+<h3>... e enviei para um servidor linux usando scp e dps vou enviar para o Github</h3>
 </body>
 </html> 

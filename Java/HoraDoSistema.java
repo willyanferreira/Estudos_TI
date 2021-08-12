23f70cfd88f099c54cde386998ec4103b08f865c
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2021-08-12 10:13:47 -0300

    Delete HoraDoSistema.java

--- Java/HoraDoSistema.java
+++
@@ -1,8 +0,0 @@
-import java.util.Date;
-
-public class HoraDoSistema {
-    public static void main(String[] args) {
-        Date data = new Date();
-        System.out.print("A hora do sistema é: " + data.toString() + ", tá massa!");
-    }
-}

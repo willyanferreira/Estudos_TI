cefe092a001f408209f0e4412002d3bc1bea6ed8
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2021-08-12 10:16:07 -0300

    Create PrimeiroPrograma.java

---
+++ Java/src/PrimeiroPrograma.java
@@ -0,0 +1,7 @@
+// package primeiroprograma;
+    public class PrimeiroPrograma{
+        public static void main(String[] args) {
+            System.out.println("Bom dia!!!\nAtualizou\tna hora."); //println imprime na tela e pula para a linha de baixo. print imprime e continua na mesma linha
+            
+        }
+    }

4b24335aa16bc915ea05bb535e5687e4754b4723
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2021-08-12 10:15:59 -0300

    Create HoraDoSistema.java

---
+++ Java/src/HoraDoSistema.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class HoraDoSistema {
+    public static void main(String[] args) {
+        Date data = new Date();
+        System.out.print("A hora do sistema é: " + data.toString() + ", tá massa!");
+    }
+}

e28a4ac056e9b7874badf8c889c57055fb5c1d81
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-03-18 00:03:32 -0300

    Update exercicios.py
    Praticando Python

--- Python/exercicios.py
+++ Python/exercicios.py
@@ -88,3 +88,14 @@ pygame.event.wait()"""
 # r = float(input('Digite o valor do raio: \n'))
 # a = float(PI * (math.pow(r,2)))
 # print("O valor da área é:\n",a)
+
+"""lendo livro de algoritimos e praticando python 18/03/22"""
+
+# HT = int(input('Informe quantas horas você trabalho esse mês:\n'))
+# VH = float(input('Informe o valor da hora/aula:\n'))
+# PD = float(input('Informe o percentual de desconto:\n'))
+# SB = float(HT * VH)
+# TD = float(PD / 100)
+# SL = float(SB - TD)
+
+# print('Seu salário bruto é:', SB, 'e seu salário líquido é:',SL)

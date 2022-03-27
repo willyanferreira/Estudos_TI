bb25ac78e6e7ac49a4d660d6045a7def27d1c43b
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-03-27 09:31:34 -0300

    Praticando JS
    - Arquivos .html, .js e .css criados.
    - Programa pra calcular o salário com base em algumas informações.

---
+++ FullStack_Web_Development/index.js
@@ -0,0 +1,19 @@
+//Exercícios de Javascript
+//Programa de cálculo de salário 27/03/22
+
+/*let ht, vh, pd, td, sb, sl;
+ht = prompt('Digite quantas horas você trabalhos esse mês:');
+vh = prompt('Digite o valor da sua hora de trabalho:');
+pd = prompt('Digite o percentual de desconto:');
+sb = ht * vh;
+td = (pd / 100) * sb;
+sl = sb - td;
+if(ht == ''){
+    console.log('Você não informou a quantidade de horas trabalhadas')
+}else if(vh == ''){
+    console.log('Você não informou o valor da sua hora de trabalho')
+}else if(pd == ''){
+    console.log('Você não informou o percentual de desconto')
+}else{
+    console.log('Seu salário base é: ' + sb + ' e seu salário líquido é: ' + sl);
+}*/ // FIM

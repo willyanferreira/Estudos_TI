964c41b62d0bddd20bd3829eecb052b29229468c
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-03-28 23:10:35 -0300

    praticando js
    - programa que calcula o volume de uma cilindro.
    - programa que calcula a qntd de  combustível gasta em uma viagem.
    - programa que calcula o valor de uma prestação em atraso.

--- FullStack_Web_Development/index.js
+++ FullStack_Web_Development/index.js
@@ -18,7 +18,7 @@ if(ht == ''){
     console.log('Seu salário base é: ' + sb + ' e seu salário líquido é: ' + sl);
 }*/
 
-// FIM
+//FIM
 
 //Programa para converter de Celsius para Fahrenheit e vice-versa 27/03/22
 
@@ -45,5 +45,41 @@ a = b;
 b = c;
 console.log('Trocando os valores das variáveis...')
 console.log('O valor de A é: \n' + a + '\nO valor de B é: \n' + b);*/
+
+//FIM
+
+// Programa para calcular volume 28/03/22
+
+/*const PI = 3.14;
+let raio = prompt('Digite o raio');
+let altura = prompt('Digite a altura');
+let volume = (PI * (raio ** 2)) * altura;
+console.log(volume);*/
+
+//FIM
+
+// Programa para calcular a qntd de lt de combustível gasto em uma viagem 28/03/22
+
+/*let tempo, distancia, velocidade, litros_usados;
+tempo = prompt('Digite o tempo gasto na viagem em minutos:')
+velocidade = prompt('Digite a velocidade média utilizada:')
+const KM_por_LT = 12;
+distancia = tempo * velocidade;
+litros_usados = distancia / KM_por_LT;
+console.log('Seu veículo se movimentou a uma velocidade média de \ ' + velocidade + '\
+km/h e  levou um tempo total de ' + tempo + ' hora(s) e percorreu uma distância de ' + distancia + '\
+km e utilizou ' + litros_usados + ' litros de combustível.'
+);*/
+
+//FIM
+
+// Programa para calcular de uma prestação em atraso 28/03/22
+
+/*let prestacao, valor, taxa, tempo;
+valor = parseInt(prompt('Digite o valor original da prestação:'));
+taxa = parseInt(prompt('Digite o percentual da taxa de juros:'));
+tempo = parseInt(prompt('Digite a quantidade de dias em atraso:'));
+prestacao = valor + (valor * ((taxa / 100) * tempo));
+console.log("O valor da prestção atualizada com juros é: R$" + prestacao);*/
 
 //FIM

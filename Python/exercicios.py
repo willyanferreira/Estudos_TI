84234db1dd4b544a44ee575d65aca13cb35c3add
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-04-21 17:57:42 -0300

    Praticando Python
    - Lendo livro de Algoritmos e praticando Python 21/04/22.
    - Programa que reajusta um salário segundo tomada de decisão.
    - Estrutura de tomada de decisão encadeada com decisão composta.

--- Python/exercicios.py
+++ Python/exercicios.py
@@ -1,4 +1,4 @@
-"""lendo livro de algoritimos e praticando python 17/03/22"""
+"""lendo livro de Algoritimos e praticando python 17/03/22"""
 
 # import math
 # PI = 3.14
@@ -6,13 +6,27 @@
 # a = float(PI * (math.pow(r,2)))
 # print("O valor da área é:\n",a)
 
-"""lendo livro de algoritimos e praticando python 18/03/22"""
+"""lendo livro de Algoritimos e praticando python 18/03/22"""
 
-HT = int(input('Informe quantas horas você trabalhou esse mês:\n'))
-VH = float(input('Informe o valor da hora/aula:\n'))
-PD = float(input('Informe o percentual de desconto:\n'))
-SB = float(HT * VH)
-TD = float(PD / 100)
-SL = float(SB - (SB * TD))
+# HT = int(input('Informe quantas horas você trabalhou esse mês:\n'))
+# VH = float(input('Informe o valor da hora/aula:\n'))
+# PD = float(input('Informe o percentual de desconto:\n'))
+# SB = float(HT * VH)
+# TD = float(PD / 100)
+# SL = float(SB - (SB * TD))
 
-print('Seu salário bruto é:', SB, 'e seu salário líquido é:',SL)
+# print('Seu salário bruto é:', SB, 'e seu salário líquido é:',SL)
+
+"""lendo livro de Algoritimos e praticando python 21/04/22"""
+
+# SA = int(input('Digite seu salário:\n'));
+# if SA < 500:
+#   NS = SA + (SA * 0.15)
+#   print(NS)
+# else:
+#   if SA <= 1000:
+#     NS = (SA + (SA * 0.10))
+#     print(NS)
+#   else:
+#     NS = SA + (SA * 0.05)
+#     print(NS)

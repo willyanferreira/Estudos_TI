0c03c99231310e0b78d1adb72e1cb977395a2bb3
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-04-22 23:14:41 -0300

    praticando JS
    - Switch

--- FullStack_Web_Development/index.js
+++ FullStack_Web_Development/index.js
@@ -326,3 +326,46 @@ raiz = `O índice da raiz é ${indice} e sua base é ${base}`;
 console.log(raiz);*/
 
 //FIM
+
+let x = parseInt(prompt("Digite um número inteiro entre 1 e 12 para saber o mês que esse número representa:"));
+
+switch(x){
+    case 1:
+        console.log('Janeiro');
+        break;
+    case 2:
+        console.log('Fevereiro');
+        break;
+    case 3:
+        console.log('Março');
+        break;
+    case 4:
+        console.log('Abril');
+        break;
+    case 5:
+        console.log('Maio');
+        break;
+    case 6:
+        console.log('Junho');
+        break;
+    case 7:
+        console.log('Julho');
+        break;
+    case 8:
+        console.log('Agosto');
+        break;
+    case 9:
+        console.log('Setembro');
+        break;
+    case 10:
+        console.log('Outubro');
+        break;
+    case 11:
+        console.log('Novembro');
+        break;
+    case 12:
+        console.log('Dezembro');
+        break;
+    default:
+        console.log('Você não digitou um número entre 1 e 12')
+}

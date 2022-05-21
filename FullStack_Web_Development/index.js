43dead04c019cc9674889c4af11e20e051f1e875
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-05-21 20:48:03 -0300

    Praticando JS
    Throw, Try, Catch and Finally

--- FullStack_Web_Development/index.js
+++ FullStack_Web_Development/index.js
@@ -455,3 +455,10 @@ if((s1 == "M" && (!(s2 == "M"))) || (!(s1 == "M") && s2 == "M")){ // outra forma
 }*/
 
 //FIM
+
+let dia = new Date().getHours();
+try{
+    console.losg(dia);
+}catch(sabado){
+    console.log(`O que está acontecendo: ${sabado.message}`);
+}
